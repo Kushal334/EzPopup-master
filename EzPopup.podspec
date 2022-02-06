7d58d8536f928272a3baa48b499f44b0fdcf9d93
Kushal Shingote <kushalshingote2@gmail.com>
2022-02-06 13:31:07 +0530

    Add files via upload

---
+++ EzPopup.podspec
@@ -0,0 +1,18 @@
+Pod::Spec.new do |s|
+  s.name             = 'EzPopup'
+  s.version          = '1.2.3'
+  s.summary          = 'EzPopup will help you to show a popup in the simplest way.'
+
+
+
+  s.description      = 'EzPopup will help you to show your custom popup. You just need to provide your view or view controller.'
+
+  s.homepage         = 'https://github.com/huynguyencong/EzPopup'
+  s.license          = { :type => 'MIT', :file => 'LICENSE' }
+  s.author           = { 'huynguyencong' => 'conghuy2012@gmail.com' }
+  s.source           = { :git => 'https://github.com/huynguyencong/EzPopup.git', :tag => s.version.to_s }
+
+  s.ios.deployment_target = '13.0'
+
+  s.source_files = 'EzPopup/Classes/**/*'
+end
---
+++ MainViewController.swift
@@ -0,0 +1,94 @@
+//
+//  MainViewController.swift
+//  EzPopup
+//
+//  Created by huynguyencong on 06/04/2018.
+//  Copyright (c) 2018 huynguyencong. All rights reserved.
+//
+
+import UIKit
+import EzPopup
+
+class MainViewController: UIViewController {
+    
+    let customAlertVC = CustomAlertViewController.instantiate()
+    let pickerVC = NumberPickerViewController.instantiate()
+    
+    @IBOutlet weak var bottomButton: UIButton!
+    
+    // MARK: - Actions
+
+    @IBAction func showAtCenterButtonTapped(_ sender: Any) {
+        guard let customAlertVC = customAlertVC else { return }
+        
+        customAlertVC.titleString = "Congratulation"
+        customAlertVC.messageString = "You just showed a custom alert view controller."
+        
+        let popupVC = PopupViewController(contentController: customAlertVC, popupWidth: 300, popupHeight: nil)
+        popupVC.cornerRadius = 5
+        popupVC.delegate = self
+        present(popupVC, animated: true, completion: nil)
+    }
+    
+    @IBAction func showTopRightButtonTapped(_ sender: Any) {
+        guard let pickerVC = pickerVC else { return }
+        
+        pickerVC.delegate = self
+        
+        let popupVC = PopupViewController(contentController: pickerVC, position: .topRight(CGPoint(x: 16, y: 40)), popupWidth: 100, popupHeight: 200)
+        popupVC.canTapOutsideToDismiss = false
+        popupVC.cornerRadius = 5
+        present(popupVC, animated: true, completion: nil)
+    }
+    
+    @IBAction func showFullCustomPopupButtonTapped(_ sender: Any) {
+        guard let pickerVC = pickerVC else { return }
+        
+        pickerVC.delegate = self
+        
+        let popupVC = PopupViewController(contentController: pickerVC, position: .bottom(20), popupWidth: 200, popupHeight: 300)
+        popupVC.backgroundAlpha = 0.3
+        popupVC.backgroundColor = .black
+        popupVC.canTapOutsideToDismiss = true
+        popupVC.cornerRadius = 10
+        popupVC.shadowEnabled = true
+        popupVC.delegate = self
+        present(popupVC, animated: true, completion: nil)
+    }
+    
+    @IBAction func showPopUpBelowButtonTapped(_ sender: Any) {
+        guard let pickerVC = pickerVC else { return }
+        
+        pickerVC.delegate = self
+        
+        let popupVC = PopupViewController(
+            contentController: pickerVC,
+            position: .offsetFromView(CGPoint(x: 0, y: bottomButton.frame.height + 3), bottomButton),
+            popupWidth: 100,
+            popupHeight: 200
+        )
+        
+        popupVC.canTapOutsideToDismiss = false
+        popupVC.cornerRadius = 5
+        present(popupVC, animated: true, completion: nil)
+    }
+}
+
+extension MainViewController: NumberPickerViewControllerDelegate {
+    func numberPickerViewController(sender: NumberPickerViewController, didSelectNumber number: Int) {
+        dismiss(animated: true) {
+            let alertController = UIAlertController(title: "Good luck", message: "Good luck with your number \(number)", preferredStyle: .alert)
+            
+            let closeButton = UIAlertAction(title: "Close", style: .cancel)
+            alertController.addAction(closeButton)
+            
+            self.present(alertController, animated: true)
+        }
+    }
+}
+
+extension MainViewController: PopupViewControllerDelegate {
+    func popupViewControllerDidDismissByTapGesture(_ sender: PopupViewController) {
+        print("log - popupViewControllerDidDismissByTapGesture")
+    }
+}
